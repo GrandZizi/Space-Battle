fc68d82fa04a31b61d82711df47c311645949e9b
GrandZizi <josmarbm@gmail.com>
2021-02-23 19:41:29 -0500

    added canvas for better UI representation

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
+class Alien {
+	//values can be changed to vary types of beams
+	constructor(x, y, radius, color, velocity) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.color = color;
+		this.velocity = velocity;
+	}
+
+	draw() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+		c.fillStyle = this.color;
+		c.fill();
+	}
+	//when called it will redraw the beam and increase its position
+	update() {
+		this.draw();
+		this.x += this.velocity.x;
+		this.y += this.velocity.y;
+	}
+}
+
 const alienFleet = [
 	{
 		type: 'MotherShip',
 		hull: 12,
 		firepower: 6,
 		accuracy: null,
+		drawing: new Alien(1,1,1,'blue',{x:1,y:1})
 	},
 	{
 		type: 'heavy',
@@ -24,7 +49,7 @@ const alienFleet = [
 		accuracy: null,
 	},
 ];
-
+console.log(alienFleet[0].drawing);
 //range for the arr
 let range = Math.floor(Math.random() * 5) + 4;
 // console.log(`range ${range}`);
@@ -54,10 +79,9 @@ for (let i = 0; i < arrSpace; i++) {
 const player = {
 	hull: 30,
 	firepower: 5,
-	accuracy: 7,
-	// shield: 5,
-	// 'max-shield': 15
+	accuracy: 7
 };
+const beams = [];
 
 //shooter function works by decreasing the range of numbers that can be generated
 //the set range will be 10, so if accuracy is 7 the range of numbers is 3 (8-10)
@@ -76,124 +100,165 @@ function shooter(acc, ship, enemyShip) {
 	}
 }
 
-// let yesOrNo = prompt('Welcome to Space Battle, ready to play? (yes/no)');
-// while (yesOrNo === 'yes') {
-
-// 	for (let i = alienFleet.length - 1; i >= 0; i--) {
-// 		//runs code block if ship still has health
-// 		if (player.hull > 0) {
-// 			while (alienFleet[i].hull >= 0) {
-// 				//randomly set alien accuracy to 5-8
-// 				alienFleet[i].accuracy = Math.floor(Math.random() * 3) + 5;
-// 				console.log(` alien acc : ${alienFleet[i].accuracy}`);
-
-// 				//should return damage of player ship
-// 				let playerAction = shooter(player.accuracy, player, alienFleet[i]);
-// 				console.log(`playerAction ${playerAction}`);
-
-// 				//subtracts health for alien
-// 				alienFleet[i].hull -= playerAction;
-// 				console.log(` alien health: ${alienFleet[i].hull}`);
-
-// 				//checks if alien has health
-// 				if (alienFleet[i].hull <= 0) {
-// 					console.log('enemy ship destroyed');
-// 					console.log('-------------------------------');
-// 					//breaks while loop if alien has no health
-// 					break;
-// 				} else {
-// 					//should return alien ship firepower if chance matches
-// 					let cpuAction = shooter(
-// 						alienFleet[i].accuracy,
-// 						alienFleet[i],
-// 						player
-// 					);
-// 					console.log(`cpuAction : ${cpuAction}`);
-
-// 					//suntracts health for player
-// 					player.hull -= cpuAction;
-// 					if (player.hull <= 0) {
-// 						alert('Oh no!! You died');
-// 						break;
-// 					}
-// 					console.log(` player health: ${player.hull}`);
-// 					console.log(`alienFlet[${i}]`);
-// 					console.log('-------------------------------');
-// 				}
-// 			}
-// 			//tells what the ship index is
-
-// 		} else {
-// 			//if no health, else block breaks loop
-// 			break;
-// 		}
-// 	}
-// 	//paths for when for-loop completes
-// 	if (player.hull > 0) {
-// 		yesOrNo = prompt('You saved Earth! Wanna do it again? (yes/no)');
-// 	} else {
-// 		yesOrNo = prompt('Earth is destroyed. Try again? (yes/no)');
-// 	}
-// }
-
 // ===============================================================================
 const dialogueText = document.querySelector('.dialogue-text');
-const contBtn = document.querySelector('.continue')
-const dialogue = document.querySelector('.dialogue')
-const action = document.querySelector('.action')
-const playerHealthText = document.querySelector('#player-health')
-const alienShipHealth = document.querySelector("#alien-health")
-const hitText = document.querySelector('#hit-text')
-const missedText = document.querySelector('#missed-text')
-const alienDestroyedText = document.querySelector('#alienship-destroyed')
+const contBtn = document.querySelector('.continue');
+const dialogue = document.querySelector('.dialogue');
+const action = document.querySelector('.action');
+const playerHealthText = document.querySelector('#player-health');
+const alienShipHealth = document.querySelector('#alien-health');
+const hitText = document.querySelector('#hit-text');
+const missedText = document.querySelector('#missed-text');
+const alienDestroyedText = document.querySelector('#alienship-destroyed');
 const playerDestroyedText = document.querySelector('#player-destroyed');
+const attackbtn = document.querySelector('.attack-button')
 
 const texts = [
 	'There is an alien fleet heading to Earth. Only you can prevent this armaggedon. The alien fleet consist of different type ships: scouts, rapid fire, cannon and The Mother-Ship',
 	'After you destroy each ship, another one will appear in order to defend the mother ship',
 	'Your ship is strong but not invincible, it can handle 20 points of damage. After destroying each ship you can choose to either continuing the fight or flee and hope they see us as a threat and flee',
-	"Ready to kick alien ass?"
+	'Ready to kick alien ass?',
 ];
 
-let textCount=0;
-contBtn.addEventListener('click', e => { 
-    dialogueText.innerText = texts[textCount]
-    if (textCount >= 4) {
-       dialogue.style.display = 'none'
-       action.style.display = 'block'
-    }
-    textCount++;
-})
+let textCount = 0;
+contBtn.addEventListener('click', (e) => {
+	dialogueText.innerText = texts[textCount];
+	if (textCount >= 4) {
+		dialogue.style.display = 'none';
+		action.style.display = 'block';
+	}
+	textCount++;
+});
 
-console.log(alienFleet);
+// console.log(alienFleet);
 shipsIndex = alienFleet.length - 1;
-// playerHealthText=player.hull
 
-action.addEventListener('click', () => {
-    playerHealthText.innerText=player.hull
-    alienShipHealth.innerText=alienFleet[shipsIndex].hull
-    let playerAction = shooter(player.accuracy, player, alienFleet[shipsIndex]);
-    alienFleet[shipsIndex].hull -= playerAction;
 
-    if (playerAction === 0) {
-        hitText.style.display='none'
-        missedText.style.display='block'
-    }else{
-        missedText.style.display='none'
-        hitText.style.display='block'
-    }
+attackbtn.addEventListener('click', e => {
+	playerHealthText.innerText = player.hull;
+	alienShipHealth.innerText = alienFleet[shipsIndex].hull;
+	let playerAction = shooter(player.accuracy, player, alienFleet[shipsIndex]);
+	
 
-    if (alienFleet[shipsIndex].hull <= 0) {
-        alienDestroyedText.style.display='block'
-        shipsIndex--;
-    }else{
-        alienFleet[shipsIndex].accuracy = Math.floor(Math.random() * 4) + 5;
-        let cpuAction = shooter(alienFleet[shipsIndex].accuracy,alienFleet[shipsIndex],player);
-        console.log(cpuAction);
-        player.hull -= cpuAction;
+	if (playerAction === 0) {
+		beams.push(new Beam(canvas.width/6, canvas.height/2, 3, 'yellow', {x:1,y:Math.ceil((Math.random()-0.5)*10)}));
+		hitText.style.display = 'none';
+		missedText.style.display = 'block';
+	} else {
+		beams.push(new Beam(canvas.width/6, canvas.height/2, 3, 'yellow', {x:1,y:0}));
+		alienFleet[shipsIndex].hull -= playerAction;
+		missedText.style.display = 'none';
+		hitText.style.display = 'block';
+	}
 
-        if (player.hull <= 0) {
-            playerDestroyedText.style.display = 'block'
-        }
-    }
-})
+	if (alienFleet[shipsIndex].hull <= 0) {
+		alienDestroyedText.style.display = 'block';
+		shipsIndex--;
+	} else {
+		
+		alienFleet[shipsIndex].accuracy = Math.floor(Math.random() * 4) + 5;
+		let cpuAction = shooter(
+			alienFleet[shipsIndex].accuracy,
+			alienFleet[shipsIndex],
+			player
+		);
+
+		console.log(cpuAction);
+		if (cpuAction === 0) {
+			beams.push(new Beam(canvas.width - canvas.width/6, canvas.height/2, 3, 'blue', {x:-1,y:Math.ceil((Math.random()-0.5)*10)}));
+			
+		}else{
+			player.hull -= cpuAction;
+			beams.push(new Beam(canvas.width - canvas.width/6, canvas.height/2, 3, 'blue', {x:-1,y:0}));
+		}
+		
+
+		if (player.hull <= 0) {
+			playerDestroyedText.style.display = 'block';
+		}
+	}
+});
+
+// ===================================================================
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+class Player {
+	constructor(x, y, radius, color) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.color = color;
+	}
+
+	draw() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+		c.fillStyle = this.color;
+		c.fill();
+	}
+}
+
+class Beam {
+	//values can be changed to vary types of beams
+	constructor(x, y, radius, color, velocity) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.color = color;
+		this.velocity = velocity;
+	}
+
+	draw() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+		c.fillStyle = this.color;
+		c.fill();
+	}
+	//when called it will redraw the beam and increase its position
+	update() {
+		this.draw();
+		this.x += this.velocity.x;
+		this.y += this.velocity.y;
+	}
+}
+
+// class Alien {
+// 	//values can be changed to vary types of beams
+// 	constructor(x, y, radius, color, velocity) {
+// 		this.x = x;
+// 		this.y = y;
+// 		this.radius = radius;
+// 		this.color = color;
+// 		this.velocity = velocity;
+// 	}
+
+// 	draw() {
+// 		c.beginPath();
+// 		c.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+// 		c.fillStyle = this.color;
+// 		c.fill();
+// 	}
+// 	//when called it will redraw the beam and increase its position
+// 	update() {
+// 		this.draw();
+// 		this.x += this.velocity.x;
+// 		this.y += this.velocity.y;
+// 	}
+// }
+
+
+let animateId;
+function animate() {
+	animateId = requestAnimationFrame(animate);
+	c.fillStyle = 'rgba(0,0,0,0.1)';
+	c.fillRect(0, 0, 1000, 1000);
+	let player = new Player(canvas.width / 6, canvas.height / 2, 10, 'red');
+	player.draw();
+	let alien = new Alien(canvas.width - canvas.width/6, canvas.height / 2, 10, 'purple');
+	alien.draw();
+	
+
+	beams.forEach(beam => beam.update())
+}
+animate();
